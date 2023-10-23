9a363935fee8298cd2509131736aeaab3d0339af
Nachop51 <nachoperalta0@gmail.com>
2023-10-23 01:10:56 -0300

    socket config

--- server/index.js
+++ server/index.js
@@ -8,7 +8,25 @@ const port = process.env.PORT ?? 3000;
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {
+    maxDisconnectionDuration: 3000
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+
+    io.emit('chat message', 'Hello there from the server!');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
 
 app.use(logger('dev'));
 
@@ -16,6 +34,6 @@ app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
 });
