4e2752655d480911750bf0172d1116285fb8c53e
Nachop51 <nachoperalta0@gmail.com>
2023-10-24 00:17:34 -0300

    live chat

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import logger from 'morgan';
+import dotenv from 'dotenv';
+import { createClient } from '@libsql/client';
 
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
 
 const port = process.env.PORT ?? 3000;
 
+dotenv.config();
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
@@ -14,18 +17,62 @@ const io = new Server(server, {
   }
 });
 
-io.on('connection', (socket) => {
+const db = createClient({
+  url: 'libsql://accepted-blok-nachop51.turso.io',
+  authToken: process.env.DB_TOKEN
+});
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT NOT NULL,
+    user TEXT
+  );
+`);
+
+io.on('connection', async (socket) => {
   console.log('A user connected');
-
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-
-    io.emit('chat message', 'Hello there from the server!');
-  });
 
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
+
+  socket.on('chat message', async (msg, username) => {
+    let result;
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content, username) VALUES (:msg, :username)',
+        args: {
+          msg,
+          username
+        }
+      });
+    } catch (err) {
+      console.error(err);
+      return;
+    }
+
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username);
+  });
+
+  // console.log(socket.handshake.auth);
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: 'SELECT * FROM messages WHERE id > :id',
+        args: {
+          id: [socket.handshake.auth.serverOffset ?? 0]
+        }
+      });
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString(), row.username);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
 });
 
 app.use(logger('dev'));
